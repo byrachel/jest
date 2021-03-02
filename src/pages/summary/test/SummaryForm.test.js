eac282bd9e91edb438bea6f337a6e8c9f29a2770
Rachel Nething <r.nething@e4-ia.fr>
2021-03-02 14:18:05 +0100

    msw

--- src/pages/summary/test/SummaryForm.test.js
+++ src/pages/summary/test/SummaryForm.test.js
@@ -1,5 +1,11 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  fireEvent,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("Initial conditions", () => {
   render(<SummaryForm />);
@@ -27,7 +33,28 @@ test("check if the first click enable button and disable on a second click", () 
   fireEvent.click(checkbox);
   expect(confirmButton).toBeEnabled();
 
-  // click on checkbox
-  fireEvent.click(checkbox);
+  //   fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(confirmButton).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+  // popover starts out hidden
+  //query -> command to use chen you expect something NOT to be in the DOM
+  const noPopover = screen.queryByText(/no ice cream actually delivered/i);
+  expect(noPopover).not.toBeInTheDocument();
+
+  // popover appears upon mouseover the label
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  // simulate mouse hover
+  userEvent.hover(termsAndConditions);
+  const popover = screen.getByText(/no ice cream actually delivered/i);
+  expect(popover).toBeInTheDocument();
+
+  //popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream actually delivered/i)
+  );
 });
